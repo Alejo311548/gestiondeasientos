1d9ed8a8ed1d59bcd9fe5e61712192e11cf798ef
Alejo311548 <alejandro.vargaso@udea.edu.co>
2024-10-14 12:47:46 -0500

    recent commit

--- src/test/java/com/aerolinea/gestiondeasientos/GestiondeasientosApplicationTests.java
+++ src/test/java/com/aerolinea/gestiondeasientos/GestiondeasientosApplicationTests.java
@@ -1,4 +1,4 @@
-package com.aerolinea.gestiondeasientos; 
+package com.aerolinea.gestiondeasientos;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
